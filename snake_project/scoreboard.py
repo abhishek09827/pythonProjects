daa824508a4c61db640c2637f272def9cf244340
abhishek09827 <107637702+abhishek09827@users.noreply.github.com>
2022-10-10 00:13:53 +0530

    Add files via upload

---
+++ snake_project/scoreboard.py
@@ -0,0 +1,43 @@
+from turtle import Turtle,Screen
+from snake import Snake
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.high_score= 0
+        self.screen = Screen()
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score}, HighScore{self.high_score}", align=ALIGNMENT, font=FONT)
+
+
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    #
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+
